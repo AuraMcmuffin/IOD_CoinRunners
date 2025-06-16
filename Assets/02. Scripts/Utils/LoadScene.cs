4e97abfc624e4b4679d52867334d698d06c7aa32
Carla Tort <carlatm2003@gmail.com>
2025-06-16 16:05:32 +0200

    Actualización_Menú y dios

--- Assets/02. Scripts/Utils/LoadScene.cs
+++ Assets/02. Scripts/Utils/LoadScene.cs
@@ -3,8 +3,43 @@ using UnityEngine.SceneManagement;
 
 public class LoadScene : MonoBehaviour
 {
-    public void ReloadCurrentScene()
+    public string sceneName;
+    public GameObject howToPlayPanel; // Arrástralo en Inspector
+
+    public void ChangeScene()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        if (!string.IsNullOrEmpty(sceneName))
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+        else
+        {
+            Debug.LogWarning("sceneName no está seteada.");
+        }
+    }
+  
+    public void HowToPlayPanel()
+    {
+        if (howToPlayPanel != null)
+        {
+            howToPlayPanel.SetActive(true);
+        }
+        else
+        {
+            Debug.LogWarning("HowToPlay panel no está referenciado.");
+        }
+    }
+  
+    public void CloseHowToPlay()
+    {
+        if (howToPlayPanel != null)
+        {
+            howToPlayPanel.SetActive(false);
+        }
+        else
+        {
+            Debug.LogWarning("HowToPlay panel no está referenciado.");
+        }
     }
 }
+
